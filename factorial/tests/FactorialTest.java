aedc3d3f601cf750fa7a15b1d2acb7ef9bda716e
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-09-20 17:07:49 -0300

    Adicionando classe de teste FactorialTest
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ factorial/tests/FactorialTest.java
@@ -0,0 +1,14 @@
+package factorial.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FactorialTest {
+	
+	@Test
+	public void testValidIntegerFactorial() {
+		Factorial fac = new Factorial(5);
+		assertEquals(120, fac.result());
+	}
+}
