e6eed019011a3a818e7478f85e59c7e1e40840c2
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-09-20 17:17:24 -0300

    Adicionando Factorial class
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ factorial/Factorial.java
@@ -0,0 +1,13 @@
+package factorial;
+
+public class Factorial {
+	private int number;
+	
+	public Factorial(int number) {
+		this.number = number;
+	}
+	
+	public int result() {
+		return 120;
+	}
+}
