ba218dcf8812a92525ffb278f20c5a6aa8dbd7a9
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-09-20 22:26:04 -0300

    Adicionando mais valores válidos cujo resultado excede o tamanho de um int
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- factorial/tests/ValidFactorialParameterizedTest.java
+++ factorial/tests/ValidFactorialParameterizedTest.java
@@ -18,27 +18,35 @@ public class ValidFactorialParameterizedTest {
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-			{0, 1},
-			{1, 1},
-			{2, 2},
-			{3, 6},
-			{4, 24},
-			{5, 120},
-			{6, 720},
-			{7, 5040},
-			{8, 40320},
-			{9, 362880},
-			{10, 3628800},
-			{11, 39916800},
-			{12, 479001600}
+			{0, 1L},
+			{1, 1L},
+			{2, 2L},
+			{3, 6L},
+			{4, 24L},
+			{5, 120L},
+			{6, 720L},
+			{7, 5040L},
+			{8, 40320L},
+			{9, 362880L},
+			{10, 3628800L},
+			{11, 39916800L},
+			{12, 479001600L},
+			{13, 6227020800L},
+			{14, 87178291200L},
+			{15, 1307674368000L},
+			{16, 20922789888000L},
+			{17, 355687428096000L},
+			{18, 6402373705728000L},
+			{19, 121645100408832000L},
+			{20, 2432902008176640000L}
 		});
 	}
 
 	private int fInput;
 
-	private int fExpected;
+	private long fExpected;
 
-	public ValidFactorialParameterizedTest(int input, int expected) {
+	public ValidFactorialParameterizedTest(int input, long expected) {
 		this.fInput = input;
 		this.fExpected = expected;
 	}
